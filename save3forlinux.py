9bbd67738ee1f04c227078c66cfd424144d89e10
wootinfor <wtqs0064@163.com>
2019-05-10 23:34:16 +0800

    for linux

---
+++ save3forlinux.py
@@ -0,0 +1,22 @@
+from wxpy import *
+from bypy import ByPy
+import os
+import time
+import datetime
+import threading
+bot = Bot(console_qr=True,cache_path=True)
+@bot.register()
+def face_msg(msg):
+    image_name = msg.file_name
+    print ('接收图片')
+    print (image_name)
+    print(msg.receive_time)
+    imagetime=str(msg.receive_time)
+    msg.get_file('~/weixin/' + msg.file_name)
+    path='~/weixin/' + msg.file_name
+    
+    bp=ByPy()
+    bp.upload(localpath= path, remotepath= 'dir_name', ondup='newcopy')
+    print('上传成功'+path)
+
+embed()
