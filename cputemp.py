e10eaefeed554dc907168db17c08bb334f39082e
wtqs0064@163.com <wtqs0064@163.com>
2019-11-11 00:00:28 +0800

    happy #

--- cputemp.py
+++ cputemp.py
@@ -1,36 +1,55 @@
-import urllib2
-import json
-import time
-import datetime
-APIKEY = 'oqSRzDP3XmZCxdShLtvEfxye54E'
-def get_temp():
-        # 打开文件 
-        file = open("/sys/class/thermal/thermal_zone0/temp") 
-        temp = float(file.read()) / 1000 
-        file.close() 
-        print "CPU的温度值为: %.3f" %temp 
-        return temp
+# import urllib2
+# import json
+# import time
+# import datetime
+# APIKEY = 'oqSRzDP3XmZCxdShLtvEfxye54E'
+# def get_temp():
+#         # 打开文件 
+#         file = open("/sys/class/thermal/thermal_zone0/temp") 
+#         temp = float(file.read()) / 1000 
+#         file.close() 
+#         print "CPU的温度值为: %.3f" %temp 
+#         return temp
         
         
-def http_put():
-    temperature = get_temp() 
-    CurTime = datetime.datetime.now()
-    url='http://api.heclouds.com/devices/533941417/datapoints'
-    values={'datastreams':[{"id":"temp","datapoints":[{"at":CurTime.isoformat(),"value":temperature}]}]}
+# def http_put():
+#     temperature = get_temp() 
+#     CurTime = datetime.datetime.now()
+#     url='http://api.heclouds.com/devices/533941417/datapoints'
+#     values={'datastreams':[{"id":"temp","datapoints":[{"at":CurTime.isoformat(),"value":temperature}]}]}
 
-    print "当前的ISO时间为： %s" %CurTime.isoformat()
-    print "上传的温度值为: %.3f" %temperature
+#     print "当前的ISO时间为： %s" %CurTime.isoformat()
+#     print "上传的温度值为: %.3f" %temperature
 
-    jdata = json.dumps(values)            
-    print jdata
-    request = urllib2.Request(url, jdata)
-    request.add_header('api-key', APIKEY)
-    request.get_method = lambda:'POST'  
-    request = urllib2.urlopen(request)
-    return request.read()
+#     jdata = json.dumps(values)            
+#     print jdata
+#     request = urllib2.Request(url, jdata)
+#     request.add_header('api-key', APIKEY)
+#     request.get_method = lambda:'POST'  
+#     request = urllib2.urlopen(request)
+#     return request.read()
 
-while True:
-        time.sleep(5)
-        resp = http_put()
-        print "OneNET请求结果:\n %s" %resp
-        time.sleep(5)
+# while True:
+#         time.sleep(5)
+#         resp = http_put()
+#         print "OneNET请求结果:\n %s" %resp
+#         time.sleep(5)
+global p
+p=1
+while 1:
+    while p<=100:
+        p+=10
+        r.asdfasdf(p)
+        g.adfasdfasdf(100-p)
+        if p<=50:
+            b.adfasdaf(50+p)
+        else:
+            b.asdfasf(150-p)
+    while p>=0:
+        p-=10
+        r.asdfasdf(p)
+        g.adfasdfasdf(100-p)
+        if p<=50:
+            b.adfasdaf(50+p)
+        else:
+            b.asdfasf(150-p)
